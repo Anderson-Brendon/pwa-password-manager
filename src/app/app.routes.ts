4de8a6b599b6474e4bbcb3a5bf1d2a2c92f06f3e
Anderson Brendon <andersonbrendonn@gmail.com>
2024-06-26 12:55:27 -0300

    update navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { PasswordListingComponent } from './password-listing/password-listing.component';
+import { PasswordDetailsComponent } from './password-details/password-details.component';
+import { UserConfigsComponent } from './user-configs/user-configs.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { CreatePasswordComponent } from './create-password/create-password.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'psw-list', component: PasswordListingComponent},
+    {path:'psw-details{id}', component:PasswordDetailsComponent},
+    {path:'user-configs', component:UserConfigsComponent},
+    {path:'create-psw', component:CreatePasswordComponent},
+    {path:'user-login', component:UserLoginComponent}
+];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { RouterLink } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTooltipModule} from '@angular/material/tooltip';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,MatButtonModule, MatTooltipModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })

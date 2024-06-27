ecfd9195537bb88f9c316277adfe787e3a324434
Anderson Brendon <andersonbrendonn@gmail.com>
2024-06-27 16:35:32 -0300

    update-user-configs

--- src/app/user-configs/user-configs.component.ts
+++ src/app/user-configs/user-configs.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormField} from '@angular/material/input';
 
 @Component({
   selector: 'app-user-configs',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,MatInputModule,MatFormField ],
   templateUrl: './user-configs.component.html',
   styleUrl: './user-configs.component.css'
 })
+
 export class UserConfigsComponent {
 
 }

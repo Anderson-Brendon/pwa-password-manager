a8fb8617d88c7f2c23ffe612846f0284eba56040
Anderson Brendon <andersonbrendonn@gmail.com>
2024-06-27 09:47:30 -0300

    update-create-password-component

--- src/app/create-password/create-password.component.ts
+++ src/app/create-password/create-password.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-create-password',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatButtonModule, RouterLink, MatDividerModule,MatFormFieldModule],
   templateUrl: './create-password.component.html',
   styleUrl: './create-password.component.css'
 })

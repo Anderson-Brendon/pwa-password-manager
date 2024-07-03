d4a83797c2ee7210400ff644ec0a1537745fea88
Anderson Brendon <andersonbrendonn@gmail.com>
2024-07-03 09:53:53 -0300

    corrigindo alguns componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { PasswordListingComponent } from './password-listing/password-listing.co
 import { PasswordDetailsComponent } from './password-details/password-details.component';
 import { UserConfigsComponent } from './user-configs/user-configs.component';
 import { UserLoginComponent } from './user-login/user-login.component';
-import { CreatePasswordComponent } from './create-password/create-password.component';
 import { authenticationGuard } from './authentication.guard';
+import { InsertAccountComponent } from './insert-account/insert-account.component';
 
 export const routes: Routes = [
     {path:'psw-list', component: PasswordListingComponent},
     {path:'psw-details/{id}', component:PasswordDetailsComponent},
     {path:'user-configs', component:UserConfigsComponent,canActivate:[authenticationGuard]},
-    {path:'create-psw', component:CreatePasswordComponent},
+    {path:'insert-account', component:InsertAccountComponent},
     {path:'', component:UserLoginComponent}
 ];

aa2d448d6aa37ea1bb3db892b32c80dd3cd8e538
Anderson Brendon <andersonbrendonn@gmail.com>
2024-07-09 09:37:44 -0300

    -update configurando-route-animations
    -adicionado animações entre rotas no app
    
    -alterações no css
    
    -crição de arquivo animations.ts para
    animações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { UserLoginComponent } from './user-login/user-login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AuthenticationService } from './authentication.service';
+import { slideInAnimation } from './animations';
+import { RouterLink } from '@angular/router';
+import { ChildrenOutletContexts } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserLoginComponent,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,RouterLink],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  animations: [
+    slideInAnimation
+  ]
 })
+
 export class AppComponent {
 
-  constructor(public authService: AuthenticationService){}
+  constructor(public authService: AuthenticationService, private contexts: ChildrenOutletContexts){}
 
   title = 'pwa-password-manager';
 
   pwa = '';
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.data?.['animation'];
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,12 +7,13 @@ import { AccountsListingComponent } from './accounts-listing/accounts-listing.co
 import { EditAccountComponent } from './edit-account/edit-account.component';
 import { PswMakerComponent } from './psw-maker/psw-maker.component';
 
+
 export const routes: Routes = [
     {path:'', redirectTo:'/user-login', pathMatch:'full'},
-    {path:'accounts-list', component: AccountsListingComponent, canActivate:[authenticationGuard]},
-    {path:'user-configs', component:UserConfigsComponent,canActivate:[authenticationGuard]},
-    {path:'insert-account', component:InsertAccountComponent, canActivate:[authenticationGuard]},
-    {path:'user-login', component:UserLoginComponent},
-    {path:'edit-account/:id', component:EditAccountComponent, canActivate:[authenticationGuard]},
-    {path:'password-maker', component: PswMakerComponent, canActivate:[authenticationGuard]}
+    {path:'accounts-list', component: AccountsListingComponent, canActivate:[authenticationGuard], data:{animation: 'AccountsListing'}},
+    {path:'user-configs', component:UserConfigsComponent,canActivate:[authenticationGuard], data:{animation: 'UserConfigs'}},
+    {path:'insert-account', component:InsertAccountComponent, canActivate:[authenticationGuard], data:{animation: 'InsertAccount'}},
+    {path:'user-login', component:UserLoginComponent, data:{animation: 'UserLogin'}},
+    {path:'edit-account/:id', component:EditAccountComponent, canActivate:[authenticationGuard], data:{animation: 'EditAccount'}},
+    {path:'password-maker', component: PswMakerComponent, canActivate:[authenticationGuard], data:{animation: 'PswMaker'}}
 ];
